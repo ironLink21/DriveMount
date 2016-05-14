3212923404984ac897b34190d0ca198c056bf9bf
Seth Bertlshofer <celestials2013@gmail.com>
2016-05-13 23:11:48 -0600

    added setTimeout function, needs testing

--- drive_mount.py
+++ drive_mount.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
-import os
-import sys
+import os, sys, signal
 from time import strftime
 from datetime import datetime
 
+def handler(signum, frame):
+    print("got signal")
+    main()
+
 def main():
+
+    # declare SIGALRM handler
+    signal.signal(signal.SIGALRM, handler)
+
     # backup drive variables
     dev = '/dev/sdb2'
     backUpDir = '/mnt/navi'
@@ -13,7 +20,6 @@ def main():
     date = datetime.now().date().strftime('%m-%d-%y')
     time = datetime.now().time().strftime('%H:%M')
     time1 = '19:50'
-    time2 = '20:00'
 
     sys.stdout = open(log_file, 'a')
 
@@ -35,18 +41,22 @@ def main():
                 print('\t*** ERROR: unmount unsuccessful ***\n\n')
         else:
             print('\tbackup in progress... [{0} {1}]\n\n'.format(date,time))
+            # start a timeout to run this again after 30min
+            signal.setitimer(signal.ITIMER_REAL, 1800)
 
     else :
-        print('##########################################################\n\tMounting drive... [{0} {1}]\n##########################################################\n'.format(date,time))
-        os.system('sudo mount ' + dev + ' ' + backUpDir)
 
-        ISMOUNTED = os.popen("grep {0} /proc/mounts | awk {1}".format(dev, "'{ print $2 }'")).read().replace('\n', '')
+        if time1 == time:
+            print('##########################################################\n\tMounting drive... [{0} {1}]\n##########################################################\n'.format(date,time))
+            os.system('sudo mount ' + dev + ' ' + backUpDir)
 
-        print('\tDirectory: %s' % ISMOUNTED)
-        if ISMOUNTED == backUpDir:
-            print('\tDrive mounted correctly [{0} {1}]\n\n'.format(date,time))
-        else:
-            print('\tDrive failed to mount [{0} {1}]\n\n'.format(date,time))
+            ISMOUNTED = os.popen("grep {0} /proc/mounts | awk {1}".format(dev, "'{ print $2 }'")).read().replace('\n', '')
+
+            print('\tDirectory: %s' % ISMOUNTED)
+            if ISMOUNTED == backUpDir:
+                print('\tDrive mounted correctly [{0} {1}]\n\n'.format(date,time))
+            else:
+                print('\tDrive failed to mount [{0} {1}]\n\n'.format(date,time))
 
     sys.exit()
 
